6e9c8d6fc17bdd40ead5751375c500d90626e42c
Nico Ekkart <nicoekkart@gmail.com>
2016-09-05 23:30:51 +0200

    Styling

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ def show_tables():
     data = pd.read_csv('all_games.txt')
     titles = ['nummer', 'reeks', 'datum', 'thuisploeg', 'bezoekers',
               'aantal_thuis', 'aantal_bezoekers', 'scheidsrechter', 'jury', 'locatie']
-    print(data.to_html)
-    return render_template('main.html',tables=[data.to_html()])
+    return render_template('main.html',tables=[
+        data.to_html(na_rep='', justify='left', index=False, float_format=lambda x: '%.0f' % x)
+    ])
 
 if __name__ == "__main__":
     app.run(debug=True)
